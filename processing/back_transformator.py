63ba631f911ee05c3a79c1b1ae417cb7fda04956
Esmedlyaev Fedor <89976445+MaxHeadrooom@users.noreply.github.com>
2024-05-15 15:39:30 +0300

    Add files via upload

--- processing/back_transformator.py
+++ processing/back_transformator.py
@@ -2,13 +2,18 @@ import img2pdf
 import os
 
 def pngs_to_pdf(png_folder, output_pdf):
-    png_files = sorted(os.listdir(png_folder))
-    png_files = [os.path.join(png_folder, file) for file in png_files if file.endswith('.jpg')]
+    png_files = [os.path.join(png_folder, file) for file in sorted(os.listdir(png_folder)) if file.endswith('.jpg')]
+
+    if not png_files:
+        raise ValueError("No .jpg files found in the specified folder")
+
     with open(output_pdf, "wb") as f:
         f.write(img2pdf.convert(png_files))
 
 def back_swap(png_folder, output_folder):
     output_pdf = os.path.join(output_folder, 'output.pdf')
+
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
+
     pngs_to_pdf(png_folder, output_pdf)
