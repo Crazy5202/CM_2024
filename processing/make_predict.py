129d553925bf4c3f10841bcfb10b27d6b2a85dc4
Esmedlyaev Fedor <89976445+MaxHeadrooom@users.noreply.github.com>
2024-05-25 13:33:36 +0300

    Add files via upload

--- processing/make_predict.py
+++ processing/make_predict.py
@@ -4,7 +4,7 @@ import os
 def final(dir_path, predict_path):
     # predict_path = 'Doc_detect/predicts/' #папка, куда нужно положить размеченные фотки
     # Создаем папку для сохранения результатов, если она не существует
-    predict_dir = os.path.join(predict_path, 'predict6') #внутри папки создаёт папки для модели номер 6
+    predict_dir = os.path.join(predict_path, 'predict6')  # внутри папки создаёт папки для модели номер 6
     os.makedirs(predict_dir, exist_ok=True)
 
     # Собираем пути к файлам изображений
@@ -12,8 +12,15 @@ def final(dir_path, predict_path):
                  (filename.endswith('.png') or filename.endswith('.jpg') or filename.endswith('.jpeg'))]
 
     # Запускаем модель на каждом изображении и сохраняем результаты
+    # счётчик найденных лэйблов
+    count: dict = {
+        'stamp': 0,
+        'agreement': 0,
+        'invoice': 0,
+        'signature': 0
+    }
     for img_file in img_files:
-
+        # print(img_file, end='\t\t')
         results = model.predict(img_file, conf=0.19, iou=0.3, verbose=False)
         # Путь для сохранения результатов
         save_path = os.path.join(predict_dir, os.path.basename(img_file))
@@ -22,4 +29,11 @@ def final(dir_path, predict_path):
         for result in results:
             result.save(save_path)
 
-    return predict_dir
+        # Получите все классы найденных объектов
+        detected_classes = [result.names[int(cls)] for result in results for cls in result.boxes.cls]
+
+        razm = False
+        if (detected_classes):
+            razm = True
+
+    return predict_dir, razm
--- processing/mstiteli_final.py
+++ processing/mstiteli_final.py
@@ -3,16 +3,17 @@ from .make_predict import final
 from .back_transformator import back_swap
 import shutil
 
-def process(path):
+def process(path) -> bool:
     target = transform(path)
 
-    path_to_converted = final(target, path)
+    path_to_converted, razm = final(target, path)
 
     back_swap(path_to_converted, path)
 
     shutil.rmtree(target)
     shutil.rmtree(path_to_converted)
+    return razm
 
 if __name__ == '__main__':
     path = input()
-    process(path)
+    result = process(path)
